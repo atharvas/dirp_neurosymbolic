d225c3e05a6da1449503dd94639bd7c7c3e8e7b0
Atharva Sehgal <atharva.sehgal@gmail.com>
2022-10-07 21:26:01 +0000

    added dataset

--- highwaydetector/dataset.py
+++ highwaydetector/dataset.py
@@ -3,6 +3,8 @@ import torch
 import requests
 import os
 from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as tf
+from PIL import Image
 
 def download_dataset(url, save_dir="data/raw/"):
     """download url to destination"""
@@ -10,6 +12,13 @@ def download_dataset(url, save_dir="data/raw/"):
     assert r.ok, "Request failed w/error {TODO}"
     with open(os.path.join(save_dir, "dataset.zip"), 'wb') as fp:
         fp.write(r.content)
+
+
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
 
 class VideoLoader(Dataset):
     @staticmethod
@@ -31,12 +40,16 @@ class VideoLoader(Dataset):
             samples.append(sequence)
         return samples
 
-
-    def __init__(self, frames_dir, max_seq_len=21):
+    def __init__(self, frames_dir, max_seq_len=21, image_size=()):
         # TODO
         self.frames_dir = frames_dir
         self.max_seq_len = max_seq_len
         assert os.path.exists(self.frames_dir)
+
+        self.src_transform = tf.Compose([
+                  tf.Resize((256, 256)),
+                  tf.ToTensor(),
+                ])
         
         self.samples = self.collate_samples(self.frames_dir, max_seq_len=self.max_seq_len)
 
@@ -50,11 +63,15 @@ class VideoLoader(Dataset):
         """
         returns [seq_len, 3, 256, 256]
         """
-        print(idx)
         # Load images with PIL
+
+        sequence = self.samples[idx]
+        out = torch.zeros(self.max_seq_len, 3, 256, 256)
+        for i, img_path in enumerate(sequence):
+          img = pil_loader(img_path)
+          out[i] = self.src_transform(img)
         
-        # Make image sizes uniform with torchvision.transform
-        
-        out = np.zeros((self.max_seq_len, 3, 256, 256))
         return out
+          
+
 
